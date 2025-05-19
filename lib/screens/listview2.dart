3cf9f86bccc683da8b0532373b9c8fa043f6ed03
ian otieno <ianotieno23@gmail.com>
2025-05-20 01:03:54 +0300

    update rowncolumn

--- lib/screens/listview2.dart
+++ lib/screens/listview2.dart
@@ -9,6 +9,48 @@ class Listview2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("Navigation Drawer"),),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+           UserAccountsDrawerHeader(
+              accountEmail: Text("ianotieno23@gmail.com"),
+              accountName: Text("Ian Otieno"),
+              currentAccountPicture: CircleAvatar(foregroundImage:AssetImage('images/dashboard.png') ,),
+              otherAccountsPictures: [
+                CircleAvatar(foregroundImage:AssetImage('images/dashboard.png') ,),
+                CircleAvatar(foregroundImage:AssetImage('images/dashboard.png') ,),
+              ],
+              ),
+              ListTile(
+                    leading:Icon(Icons.home),title: Text ("Home"),onTap: () => {},
+                  ),
+                  
+                   ListTile(
+                    leading:Icon(Icons.shopping_cart_outlined),title: Text ("Shop"),onTap: () => {},
+                  ),
+                
+                   ListTile(
+                    leading:Icon(Icons.favorite),title: Text ("Favorite"),onTap: () => {},
+                  ),
+                 Divider(),
+             Padding(
+               padding: const EdgeInsets.all(14.0),
+               child: Text("Labels"),
+               
+             ),
+              ListTile(
+                    leading:Icon(Icons.label),title: Text ("Red"),onTap: () => {},
+                  ),
+                   ListTile(
+                    leading:Icon(Icons.label),title: Text ("Blue"),onTap: () => {},
+                  ),
+                   ListTile(
+                    leading:Icon(Icons.label),title: Text ("Green"),onTap: () => {},
+                  ),
+          ],
+        ),
+      ),
       body: Center(
         child: ListView.builder(
           itemCount: product.length,
