3639550c1b52bb86a373b9e19463e106e3263d86
ian otieno <ianotieno23@gmail.com>
2025-05-20 00:27:05 +0300

    update listview

--- lib/screens/listview2.dart
+++ lib/screens/listview2.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
 
 class Listview2 extends StatelessWidget {
-  const Listview2({super.key});
+  Listview2({super.key});
+  List<String> product = ["chair", "sofa", "bed"];
+  List<String> productDetails = ["Large chair", "Medium Sofa", "Small Bed"];
+  List<int> price = [300, 400, 500];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ListView(
-          children: [
-            Padding(padding:const EdgeInsets.all(20.0),
-            child:CircleAvatar (
-             child: Text("C"),
-            ),
-            ),
-            Divider(),
-            Padding(padding:const EdgeInsets.all(8.0),
-            child:Text ("Flutter's hot reload feature helps you quickly and easily experiment, build UIs, add features, and fix bugs. Hot reload works by injecting updated source code files into the running Dart Virtual Machine (VM).  "),)
-            
-          ],
+        child: ListView.builder(
+          itemCount: product.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+             leading: CircleAvatar(child: Text(product[index][0]),),
+             title: Text(product[index]),
+             subtitle: Text(productDetails[index]),
+             trailing: Text(price[index].toString()),
+            );
+          },
         ),
       ),
     );
