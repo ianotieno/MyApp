d7a48a916b1ceab99f8ca4c2e4a295b5676519c3
ian otieno <ianotieno23@gmail.com>
2025-05-23 13:59:49 +0300

    update

--- lib/screens/form.dart
+++ lib/screens/form.dart
@@ -9,26 +9,72 @@ class MyForm extends StatefulWidget {
 }
 
 class _MyFormState extends State<MyForm> {
+  var _productName;
+  final _productController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _productController.addListener(_updateText);
+  }
+
+  @override
+  void dispose() {
+    _productController.dispose();
+    super.dispose();
+  }
+
+  void _updateText() {
+    setState(() {
+      _productName = _productController.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("My Form"), centerTitle: true),
-      body: Center(
-        child: OutlinedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return DetailsForm();
-                },
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            TextFormField(
+              controller: _productController,
+              decoration: InputDecoration(
+                labelText: "Product Name",
+                prefixIcon: Icon(Icons.verified_user_outlined),
+                border: OutlineInputBorder(),
               ),
-            );
-          },
-          style: OutlinedButton.styleFrom(minimumSize: const Size(200, 50)),
-          child: Text("Submit Form"),
+            ),
+            TextFormField(
+              controller: _productController,
+              decoration: InputDecoration(
+                labelText: "Product Name",
+                prefixIcon: Icon(Icons.verified_user_outlined),
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            myButt(context),
+          ],
         ),
       ),
+    );
+  }
+
+  OutlinedButton myButt(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return DetailsForm(productName: _productController.text);
+            },
+          ),
+        );
+      },
+      style: OutlinedButton.styleFrom(minimumSize: const Size(200, 50)),
+      child: Text("Submit Form"),
     );
   }
 }
--- lib/screens/formDetails.dart
+++ lib/screens/formDetails.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
 class DetailsForm extends StatelessWidget {
-  const DetailsForm({super.key});
-
+   DetailsForm({super.key,required this.productName});
+  String productName;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Details Screen"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Details form"),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+      ),
+      body: Container(
+        padding:EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            ListTile(
+              leading:Icon(Icons.account_balance_wallet_outlined),
+              title:Text (productName) ,
+            ),
+
+          ],
+        ),
       ),
     );
   }
